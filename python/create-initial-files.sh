a9f218f46a5a095c7a812f106bdd8f3757c148cd
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2019-10-02 22:53:27 -0300

    Improve python create-initial-files script

--- python/create-initial-files.sh
+++ python/create-initial-files.sh
@@ -2,6 +2,10 @@
 
 project_name=${1}
 curdir=$(dirname ${BASH_SOURCE[0]})
+init_file=${project_name}/__init__.py
+skip_docs=0
+
+test ! -z ${2} && skip_docs=${2}
 
 if test -z ${project_name}; then
     echo -e "\nUsage: ${0} PROJECT_NAME\n"
@@ -9,12 +13,29 @@ if test -z ${project_name}; then
 fi
 
 mkdir -p docs/{tutorial,src/index} ${project_name}/tests
-echo -e \
+if test ! -f ${init_file}; then
+    echo -e \
     "\"\"\"${project_name}\"\"\"\n\n__version__ = '0.0.1'\n" \
-    > ${project_name}/__init__.py
+    > ${init_file}
+else
+    grep -E '__version__ = .[0-9]+\.[0-9]+\.[0-9]+([0-9a-zA-Z]+)?.' \
+        ${init_file} >/dev/null 2>&1 || \
+    echo -e "\nPlease set a __version__ variable on the file '${init_file}'." || \
+    echo -e "Example: __version__ = '0.0.1'\n" || \
+    exit 1
+fi
 
 for template in $(find ${curdir}/templates -type f); do
     new_file=${template#"${curdir}/templates/"}
-    cp ${template} ${new_file}
-    sed -i -r -e "s/\{project_name\}/${project_name}/g" ${new_file}
+
+    if echo ${new_file} | grep docs >/dev/null 2>&1; then
+        test ${skip_docs} -eq 0 && cp -i ${template} ${new_file}
+    else
+        cp -i ${template} ${new_file}
+    fi
+
+    test -f ${new_file} && \
+        sed -i -r -e "s/\{project_name\}/${project_name}/g" ${new_file}
 done
+
+test $(find docs -type f | wc -l) -eq 0 && rm -r docs
